b55fd9960ac92d9ae002a50b56a72981d2b00e41
Mazin Emad <124470200+Mazin-emad@users.noreply.github.com>
2025-07-19 18:26:39 +0300

    fix: make navbar responsive again

--- components/NavItems.tsx
+++ components/NavItems.tsx
@@ -2,6 +2,9 @@
 import { cn } from "@/lib/utils";
 import { usePathname } from "next/navigation";
 import Link from "next/link";
+import { useState } from "react";
+import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+import { Button } from "./ui/button";
 
 const navItems = [
   {
@@ -24,21 +27,81 @@ const navItems = [
 
 const NavItems = () => {
   const pathname = usePathname();
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
 
   return (
-    <div className="flex items-center gap-2 md:gap-4">
-      {navItems.map((item) => (
-        <Link
-          key={item.href}
-          href={item.href}
-          className={cn(
-            "transition-colors hover:text-primary",
-            pathname === item.href && "text-primary font-semibold"
-          )}
+    <div>
+      <div className="flex items-center gap-4 max-sm:hidden">
+        {navItems.map((item) => (
+          <Link
+            key={item.href}
+            href={item.href}
+            className={cn(
+              "transition-colors hover:text-primary",
+              pathname === item.href && "text-primary font-semibold"
+            )}
+          >
+            {item.label}
+          </Link>
+        ))}
+        <SignedOut>
+          <SignInButton>
+            <Button variant="outline" className="cursor-pointer">
+              Login
+            </Button>
+          </SignInButton>
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+      </div>
+      <div className="sm:hidden">
+        <button
+          onClick={toggleMenu}
+          className="text-gray-500 hover:text-gray-600 focus:outline-none"
         >
-          {item.label}
-        </Link>
-      ))}
+          <svg viewBox="0 0 100 80" width="40" height="40">
+            <rect width="100" height="10"></rect>
+            <rect y="30" width="100" height="10"></rect>
+            <rect y="60" width="100" height="10"></rect>
+          </svg>
+        </button>
+      </div>
+      {isOpen && (
+        <div className="absolute top-15 p-4 rounded-lg w-full right-0 bg-white z-10 sm:hidden flex items-center flex-col gap-3">
+          {navItems.map((item) => (
+            <Link
+              key={item.href}
+              href={item.href}
+              onClick={toggleMenu}
+              className={cn(
+                "transition-colors hover:text-primary",
+                pathname === item.href && "text-primary font-semibold"
+              )}
+            >
+              {item.label}
+            </Link>
+          ))}
+          <SignedOut>
+            <SignInButton>
+              <Button
+                onClick={toggleMenu}
+                variant="outline"
+                className="cursor-pointer"
+              >
+                Login
+              </Button>
+            </SignInButton>
+          </SignedOut>
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
+        </div>
+      )}
     </div>
   );
 };
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,29 +1,17 @@
 import Image from "next/image";
 import NavItems from "./NavItems";
 import Link from "next/link";
-import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
-import { Button } from "./ui/button";
 
 const Navbar = () => {
   return (
-    <nav className="navbar max-sm:justify-center">
-      <Link href="/" className="max-sm:hidden">
+    <nav className="navbar">
+      <Link href="/">
         <div className="flex items-center gap-2.5 cursor-pointer">
           <Image src="/images/logo.svg" alt="logo" width={46} height={44} />
         </div>
       </Link>
-      <div className="flex items-center sm:gap-8 gap-4 max-md:justify-end">
+      <div className="flex sm:items-center sm:gap-8 gap-4">
         <NavItems />
-        <SignedOut>
-          <SignInButton>
-            <Button variant="outline" className="cursor-pointer">
-              Login
-            </Button>
-          </SignInButton>
-        </SignedOut>
-        <SignedIn>
-          <UserButton />
-        </SignedIn>
       </div>
     </nav>
   );
